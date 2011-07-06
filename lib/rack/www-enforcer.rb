360986b6de4cea74969dc43acf51048fac4fd137
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-07-06 17:12:16 -0500

    Basic redirection support

--- lib/rack/www-enforcer.rb
+++ lib/rack/www-enforcer.rb
@@ -1,13 +1,36 @@
+require "rack"
+require "rack/request"
+
 module Rack
-  module WwwEnforcer
+  class WwwEnforcer
 
     def initialize(app, options = {})
       @app = app
-      @subdomain = options[:subdomain]
+      @subdomain = options[:subdomain] || "www"
     end
 
     def call(env)
-      @app.call(env)
+      req = Request.new(env)
+      if subdomain_present?(req)
+        @app.call(env)
+      else
+        redirect_to_subdomain(req)
+      end
+    end
+
+    private
+
+    def subdomain_present?(req)
+      host = URI(req.url).host
+      host.match(/^([^.]+)\..*$/)[1] == @subdomain
+    end
+
+    def redirect_to_subdomain(req)
+      url       = URI(req.url)
+      url.host  = "#{@subdomain}.#{url.host}"
+      headers   = {'Content-Type' => 'text/html', 'Location' => url.to_s}
+
+      [301, headers, []]
     end
   end
 end
--- test/test_www-enforcer.rb
+++ test/test_www-enforcer.rb
@@ -1,5 +1,5 @@
-$:.push File.expand_path("../lib", __FILE__)
-require "www-enforcer"
+$:.push File.expand_path("../../lib", __FILE__)
+require 'rack-www-enforcer'
 
 require 'test/unit'
 require 'rack/test'
@@ -19,4 +19,16 @@ class TestWwwEnforcer < Test::Unit::TestCase
   end
   attr_writer :app
 
+  def test_redirect_to_default_subdomain
+    get "http://example.org/"
+    assert_equal "http://www.example.org/",
+      last_response.headers['Location']
+  end
+
+  def test_redirect_to_custom_subdomain
+    self.app = Rack::WwwEnforcer.new(mock_app, :subdomain => "app")
+    get "http://example.org/"
+    assert_equal "http://app.example.org/",
+      last_response.headers['Location']
+  end
 end
